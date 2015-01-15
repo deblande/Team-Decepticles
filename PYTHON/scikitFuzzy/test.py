c165218601d38c1fd5c2c20e1293754a973d3340
Adam <acornachione@alaska.edu>
2015-01-14 15:03:05 -0900

    changed functions to sigmoids and gaussian distributions. Showed plots on test.py

--- PYTHON/scikitFuzzy/test.py
+++ PYTHON/scikitFuzzy/test.py
@@ -2,24 +2,52 @@
 import numpy as np
 import pylab as pylab
 import skfuzzy as fuzz
+import matplotlib.pyplot as plt
 pylab.rcParams['figure.figsize'] = (10.0, 5.0)
 
 ########## INPUTS ########################
 #Input Universe functions
 accell_avg = np.arange(0,11,.1)
-speed_avg  = np.arange(0,11,.1)
+speed_avg  = np.arange(0,40,.1)
 
 # Input Membership Functions
 #Figure out better curves for these
-# Accelleration
-acc_calm = fuzz.gaussmf(accell_avg ,0,1.5)
-acc_normal = fuzz.gaussmf(accell_avg ,5,1.5)
-acc_aggressive = fuzz.gaussmf(accell_avg ,10,5)
 
-# speed_avg
-speed_calm = fuzz.gaussmf(speed_avg , 0,2.5) #also have trapezoid and others
-speed_normal = fuzz.gaussmf(speed_avg, 3,7)
-speed_aggressive = fuzz.gaussmf(speed_avg ,10,7.5)
+# Accelleration
+acc_calm = fuzz.sigmf(accell_avg ,1.5,-1.3)
+acc_normal = fuzz.gaussmf(accell_avg ,2,1.5)
+acc_aggressive = fuzz.sigmf(accell_avg, 3,1.3)
+
+# speed_avg 
+speed_calm = fuzz.sigmf(speed_avg , 8,-.4) #also have trapezoid and others (see test2.py)
+speed_normal = fuzz.gaussmf(speed_avg, 17.5,10) #peak at ~40 mph
+speed_aggressive = fuzz.sigmf(speed_avg ,26,.4) #above ~60 mph is aggressive
+
+
+#PLOTTING RELATIONSHIPS
+fig, ax  = plt.subplots()
+fig, bx = plt.subplots()
+ax.plot(speed_avg, speed_calm, 'r', label = "Calm")
+ax.plot(speed_avg, speed_normal, 'm', label = "Normal")
+ax.plot(speed_avg, speed_aggressive, 'b',label = "Aggressive")
+
+ax.set_ylabel('Fuzzy Membership')
+ax.set_xlabel('Fuzzy Addition')
+ax.set_ylim(-0.05, 1.05)
+ax.set_xlim(0,40)
+ax.legend()
+
+bx.plot(accell_avg, acc_calm, 'r', label = "Calm")
+bx.plot(accell_avg, acc_normal, 'm', label = "Normal")
+bx.plot(accell_avg, acc_aggressive, 'b', label = "Aggressive")
+bx.set_ylabel('Fuzzy Membership')
+bx.set_xlabel('Fuzzy Addition')
+bx.set_ylim(-0.05, 1.05)
+bx.set_xlim(0,5)
+bx.legend()
+
+plt.show()
+
 
 def speed_category(speed_in):
     speed_cat_calm = fuzz.interp_membership(speed_avg,speed_calm,speed_in) # Depends from Step 1
@@ -34,19 +62,21 @@ def accelleration_category(accelleration_in):
     return dict(calm = accelleration_cat_calm, normal = accelleration_cat_normal, aggressive = accelleration_cat_aggressive)
 
 #Exaple input variables 
-spd_in = 10.2;
+spd_in = 35.2;
 acc_in = 5.03;
 
 speed_in = speed_category(spd_in)
 accelleration_in = accelleration_category(acc_in)
 
 #Print evaluation
-print "For accell_avg" 
+print ""
+print "Fuzzy Set inclusion for inputs:"
+print "For accell_avg = ", acc_in 
 print "\tcalm: ",accelleration_in['calm']
 print "\tnormal: ",accelleration_in['normal']
 print "\taggressive: ",accelleration_in['aggressive']
 print ""
-print "For speed_avg\n\t" 
+print "For speed_avg = ", spd_in
 print "\tcalm: ",speed_in['calm']
 print "\tnormal: ",speed_in['normal']
 print "\taggressive: ",speed_in['aggressive']
--- PYTHON/scikitFuzzy/test2.py
+++ PYTHON/scikitFuzzy/test2.py
@@ -9,23 +9,34 @@ pylab.rcParams['figure.figsize'] = (10.0, 5.0)
 n_domain = np.arange(0,10,1)
 
 # Membership function for "about a #"
-about4 = fuzz.trimf(n_domain, [3.5, 4, 4.5])
-about5 = fuzz.trimf(n_domain, [4,5,6])
+#speed_calm = fuzz.gaussmf(speed_avg , 0,2.5)
 
-about9 = fuzz.fuzzy_add(n_domain,about4,n_domain,about5)
+#LOOK in skfuzzy/membership/generatemf.py for reference
+#gaussmf(x, mean, sigma):
+#gauss2mf(x, mean1, sigma1, mean2, sigma2):
+#gbellmf(x, a, b, c):
+#piecemf(x, abc):
+#sigmoid(WX, B):
+#sigmf(x, b, c):
+#trapmf(x, abcd):
+
+about2_5 = fuzz.gaussmf(n_domain, 3,2.5)
+about5 = fuzz.sigmf(n_domain,4,-4)
+
+about9 = fuzz.fuzzy_add(n_domain,about2_5,n_domain,about5)
 
 fig, ax = plt.subplots()
 
-ax.plot(n_domain, about4, 'r', label = "About 4")
+ax.plot(n_domain, about2_5, 'r', label = "About 2.5")
 ax.plot(n_domain, about5, 'm', label = "About 5")
-ax.plot(about9[0], about9[1], 'b', label = "About 9")
+#ax.plot(about9[0], about9[1], 'b', label = "About 9")
 ax.set_ylabel('Fuzzy Membership')
 ax.set_xlabel('Fuzzy Addition')
 ax.set_ylim(-0.05, 1.05);
 ax.legend()
 
 plt.show()
-
+'''
 # Crisp Number with Fuzzy Representation / Ordinary 4
 n_domain = np.arange(0,10,1)
 crisp2 = fuzz.trimf(n_domain, [2,2,2])
@@ -48,3 +59,4 @@ ax.set_xlim(0,24);
 ax.legend()
 
 plt.show()
+'''
